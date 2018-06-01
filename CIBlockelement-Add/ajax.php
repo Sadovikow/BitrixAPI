bc3b7c738f3ac6f2c0f92bb5ab2fb874ef51f72b
Sergey Sadovikov <nexxerr@gmail.com>
2018-06-01 16:40:13 +0300

    Create ajax.php

---
+++ CIBlockelement-Add/ajax.php
@@ -0,0 +1,30 @@
+<?
+define('STOP_STATISTICS', true);
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$GLOBALS['APPLICATION']->RestartBuffer();
+ 
+use Bitrix\Main\Loader;
+Loader::includeModule("iblock");
+
+if($_GET['action'] == 'createRequest') {
+    $IBLOCK = 1;
+    /* Делаем запись в инфоблок */
+    $arFields = array(
+        "ACTIVE" => "Y",
+        "IBLOCK_ID" => $IBLOCK,
+        "NAME" => 'Элемент № '.$_GET['NAME'],
+    );
+    $oElement = new CIBlockElement();
+    $idElement = $oElement->Add($arFields, false, false, true);
+    /* Свойства */	
+		foreach($_GET as $key => $property)
+		{
+			if($key != 'requestID' && $key != 'action') {
+				CIBlockElement::SetPropertyValueCode($idElement, $key, $property); 		
+			}
+		
+		}
+	/* Свойства */
+}
+	
+die();?>
